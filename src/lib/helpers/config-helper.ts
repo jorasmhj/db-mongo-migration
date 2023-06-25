ec265e1f6d48862e4ff83a69f1e1512149a1e812
Saroj <saroj@noveltytechnology.com>
2023-06-25 18:18:59 +0545

    feat(config): added support for dynamic env variables

--- src/lib/helpers/config-helper.ts
+++ src/lib/helpers/config-helper.ts
@@ -1,18 +1,20 @@
+import 'dotenv/config'
 import path from 'path'
-import { readFile } from 'fs/promises'
+import { readYamlEnvSync } from 'yaml-env-defaults'
+
 import isFileExist from '../utils/file'
 import { IConfiguration } from '../../interface'
 
 class ConfigHelper {
   async readConfig() {
-    const configFilePath = path.join(process.cwd(), 'mongo-migrate-config.json')
+    const configFilePath = path.join(process.cwd(), 'migration-config.yaml')
 
     if (!isFileExist(configFilePath)) {
       throw new Error(`Migration has not been initialized yet. Run 'mongo-migrate init' to initialize`)
     }
 
-    const file = await readFile(configFilePath, { encoding: 'utf8' })
-    return JSON.parse(file) as IConfiguration
+    const doc = readYamlEnvSync(configFilePath)
+    return doc as IConfiguration
   }
 }
 
